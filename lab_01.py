67c918b5e030de24bb0f11c9b263813cb6409c35
Zaharov Igor <lars.zaharov@gmail.com>
2018-02-22 21:17:41 +0300

    lab 1 code refactored, comments added, pyplot window running in another process

--- lab_01.py
+++ lab_01.py
@@ -5,10 +5,7 @@
 """
 
 import math
-import scipy
-import sys
 import random
-import sympy
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
@@ -24,11 +21,14 @@ def simple_iteration_method(func, start, end, fault=0.00001):
     :param fault: погрешность
     :return: tuple - float root, number of iterations
     """
-    # make func derivative
-    # P.S. dont assign to lambda!!
-    func_derivative = lambda x: (func(x + fault) - func(x)) / ((x + fault) - x)
+    # check incoming segments border is valid
+    if end <= start or func(start) * func(end) > 0:
+        raise ValueError('Borders is not correct')
+
     # find derivative local maximum
-    local_func_max = optimize.minimize_scalar(func_derivative,
+    def _func_derivative(x):
+        return (func(x + fault) - func(x)) / ((x + fault) - x)
+    local_func_max = optimize.minimize_scalar(_func_derivative,
                                               bounds=(start, end), method='bounded')
     # represent func as g(x) = x - (1 / M) * func(x)
     if local_func_max.fun > 0:
@@ -38,7 +38,8 @@ def simple_iteration_method(func, start, end, fault=0.00001):
         iterative_func = lambda c: c + (1 / local_func_max.x) * func(c)
 
     iteration_counter = 0
-    x_0 = random.uniform(start, end)
+    # select random x0 from segment
+    x_0 = float(random.uniform(start, end))
     while math.fabs(func(x_0)) > fault:
         x_0 = iterative_func(x_0)
         iteration_counter += 1
@@ -55,16 +56,20 @@ def bisection_method(func, start, end, fault=0.00001):
     :param fault: погрешность
     :return: tuple -  float root, number of iterations
     """
-    if start < end and func(start) * func(end) > 0:
+    # check incoming segments border is valid
+    if end <= start or func(start) * func(end) > 0:
         raise ValueError('Borders is not correct')
 
     iteration_counter = 0
+    # split initial segment into two parts
     x = (start + end) / 2
     while math.fabs(func(x)) > fault:
+        # case: root is on the left hand side or in right
         if func(start) * func(x) < 0:
             end = x
         else:
             start = x
+        # again split new segment into two parts
         x = (start + end) / 2
         iteration_counter += 1
 
@@ -81,54 +86,70 @@ def newton_simple_method(func, start, end, start_x, fault=0.00001):
     :param fault: погрешность
     :return: tuple - float root, number of iterations
     """
-    if start < end and func(start) * func(end) > 0:
+    # check incoming segments border is valid
+    if not start < start_x < end or func(start) * func(end) > 0:
         raise ValueError('Borders is not correct')
 
+    # using approximated derivative by the algorithm
+    def approx_func_derivative(c):
+        return (func(start_x) - func(c)) / (start_x - c)
+
     iteration_counter = 0
-    func_derivative = lambda c: (func(start_x) - func(c)) / (start_x - c)
+    # select random x from given segment
     x = float(random.uniform(start, end))
     while math.fabs(func(x)) > fault:
-        x = x - func(x) / func_derivative(x)
+        # Newtons simple method step
+        x = x - func(x) / approx_func_derivative(x)
         iteration_counter += 1
 
     return x, iteration_counter
 
 
+def _display_plot(start, end, step):
+    """ Display plot of _function(x) using pyplot """
+    # make points range
+    x = np.arange(start, end, step)                # make range from [0] to [1] with step [2]
+    y = np.array([_function(item) for item in x])  # convert list to nparray object
+
+    # configure _function plot and Y=0 line
+    plt.plot(x, y)
+    plt.plot(x, np.zeros(len(x)))
+
+    # disable interactive mod and show plot
+    plt.ioff()
+    plt.show()
+
+
+def _function(x: float):
+    """ Function, given from the task """
+    return 2 * math.log10(x) - x / 2 + 1
+
+
 def main():
-    """
-        Main logic
-        give a command line argument i to on interactive matplotlib mode
-    """
-    # given function from task
-    given_function = lambda x: 2 * math.log10(x) - x / 2 + 1
-
-    # plot making
-    x_values = np.arange(0.01, 40, 0.01)                         # make range from [0] to [1] with step [2]
-    y_values = np.array([given_function(x) for x in x_values])   # convert list to nparray object
-
-    plt.plot(x_values, y_values, label='2 * lg(x) - x / 2 + 1')  # main plot setting, not necessary to use nparrays
-    plt.plot(x_values, np.zeros(len(x_values)), label='Y')       # y=0 line setting
-
-    # TODO: Make pyplot window in another process
-    if 'i' in sys.argv:  # enable matplotlib interactive mode
-        plt.ion()        # by default interactive mode is disabled
-    plt.show()           # remember about GIL, use multiprocessing to make window clear interactive
+    """ User interaction logic """
+    # plot compact borders
+    l, r, step = 0.01, 20, 0.001
+    # start pyplot window process
+    plt_window = Process(target=_display_plot, args=(l, r, step), )
+    plt_window.start()
 
     # event loop, can be interrupt by KeyboardInterrupt Ctrl+C
     while True:
-        # users input
-        print('\nInput [start, end] of segment, where one root exist\n')
-        start, end = float(input('start: ')), float(input('end: '))
-        start_x = float(input('input x, near the root: '))
+        try:
+            # users input
+            print('\nInput [start, end] of segment, where one root exist\n')
+            start, end = float(input('start: ')), float(input('end: '))
+            start_x = float(input('input x, near the root: '))
 
-        # methods executing
-        bisection_res = bisection_method(given_function, start, end)
-        simple_newton_res = newton_simple_method(given_function, start, end, start_x)
-        iteration_res = simple_iteration_method(given_function, start, end)
-        print('bisection method: root {}, iter {}'.format(*bisection_res))
-        print('newton simple method: root {}, iter {}'.format(*simple_newton_res))
-        print('simple iteration method root {}, iter {}'.format(*iteration_res))
-
+            # methods executing
+            bisection_res = bisection_method(_function, start, end)
+            simple_newton_res = newton_simple_method(_function, start, end, start_x)
+            iteration_res = simple_iteration_method(_function, start, end)
+            print('bisection method: root {}, iter {}'.format(*bisection_res))
+            print('newton simple method: root {}, iter {}'.format(*simple_newton_res))
+            print('simple iteration method root {}, iter {}'.format(*iteration_res))
+        except Exception as e:
+            print('Exception occurred! ' + str(e))
 
 if __name__ == '__main__':
     main()
