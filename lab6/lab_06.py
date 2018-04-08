d1af4ee5a796bf57b3a08a4f2a6cd7bdd3c05d0c
Zaharov Igor <lars.zaharov@gmail.com>
2018-04-08 19:24:42 +0300

    lab 06 refactored, newton method divided on straight and inverse

--- lab6/lab_06.py
+++ lab6/lab_06.py
@@ -7,31 +7,21 @@
 """
 
 import json
-import math
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import namedtuple
+from multiprocessing import Process
 
 
-Compact = namedtuple('Compact', ['start', 'end', 'step'])
-
-
-def newton_interpolation(x, y):
+def newton_interpolation_straight(x, y):
     """
         Newton interpolation polynomial building
+        using straight interpolation formula
     :param x: range of interpolation arguments
     :param y: range of interpolation values
     :return: function representing interpolated polynomial
     """
-    f = [y, ]  # divided difference table
-
-    # divided difference table building
-    for step in range(1, len(x)):
-        differences = list()
-        for i in range(1, len(f[step - 1])):
-            differ = (f[step - 1][i] - f[step - 1][i - 1]) / (x[i + step - 1] - x[i - 1])
-            differences.append(differ)
-        f.append(differences)
+    # divided difference table
+    f = _divided_difference(x, y)
 
     def polynomial(arg):
         """ Returns interpolated polynomial value """
@@ -45,12 +35,56 @@ def newton_interpolation(x, y):
     return polynomial
 
 
-def display_plot(polynom, compact, x_interp, y_interp, args, values):
-    """ Display plot of polynom(x) using pyplot """
-    # make points range
-    x = np.arange(*compact)
-    y = np.array([polynom(item) for item in x])
+def newton_interpolation_inverse(x, y):
+    """
+        Newton interpolation polynomial building
+        using inverse interpolation formula
+    :param x: range of interpolation arguments
+    :param y: range of interpolation values
+    :return: function representing interpolated polynomial
+    """
+    # divided difference table
+    f = _divided_difference(x, y)
 
+    def polynomial(arg):
+        """ Returns interpolated polynomial value """
+        polynom_val = y[-1]
+        x_product = 1
+        for i in range(1, len(f)):
+            x_product *= arg - x[i - 1]
+            polynom_val += f[i][-1] * x_product
+        return polynom_val
+
+    return polynomial
+
+
+def _divided_difference(x, y):
+    """
+        Returns a divided difference table
+        for newton interpolation method
+    :param x: range of interpolation arguments
+    :param y: range of interpolation values
+    :return: list of lists
+    """
+    f = [y, ]  # divided difference table
+
+    # divided difference table building
+    for step in range(1, len(x)):
+        differences = list()
+        for i in range(1, len(f[step - 1])):
+            difference = (f[step - 1][i] - f[step - 1][i - 1]) / (x[i + step - 1] - x[i - 1])
+            differences.append(difference)
+        f.append(differences)
+
+    return f
+
+
+def display_plot(
+        x, y,                # interpolated polynomial points
+        x_interp, y_interp,  # interpolation nodes
+        args, values         # values from the task
+        ):
+    """ Display plot of polynom(x) using pyplot """
     plt.plot(x, y, label='interpolation')               # polynomial plot
     plt.plot(x, np.zeros(len(x)), linewidth=0.3,        # line Y=0 plot
              linestyle='--', color='red', label='Y=0')
@@ -60,10 +94,35 @@ def display_plot(polynom, compact, x_interp, y_interp, args, values):
     plt.plot(x_interp, y_interp, 'o', linewidth=0.01,
              label='interpolation nodes')
 
-    # disable interactive mode and show plot
+    # disable interactive mode
     plt.ioff()
     plt.legend(loc='best')
     plt.show()
+
+
+def _process_interpolation(interpolation, x, y, args):
+    """
+        Logic of task executing and plot building
+    :param x: range of interpolation arguments
+    :param y: range of interpolation values
+    :param args: arguments, given from the task
+    :return: None
+    """
+    # line of polynomial plot setup
+    start, end, step = 0, 0.9, 0.001
+    x_range = np.arange(start, end, step)
+    y_range = np.array([interpolation(arg) for arg in x_range])
+
+    # points, should be counted by the task
+    values = []
+    for arg in args:
+        value = interpolation(arg)
+        values.append(value)
+
+    # run pyplot window in another process
+    plot_args = x_range, y_range, x, y, args, values
+    window = Process(target=display_plot, args=plot_args)
+    window.start()
 
 
 def main():
@@ -75,15 +134,11 @@ def main():
         y = point_ranges['y']
         args = point_ranges['args']
 
-    interpolation = newton_interpolation(x, y)
+    str_interpolation = newton_interpolation_straight(x, y)
+    _process_interpolation(str_interpolation, x, y, args)
 
-    values = []
-    for arg in args:
-        value = interpolation(arg)
-        values.append(value)
-
-    compact = Compact(start=0, end=0.9, step=0.001)
-    display_plot(interpolation, compact, x, y, args, values)
+    inv_interpolation = newton_interpolation_inverse(x, y)
+    _process_interpolation(inv_interpolation, x, y, args)
 
 
 if __name__ == '__main__':
